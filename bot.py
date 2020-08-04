860446bbbe735cbeb4e91eb4405831bbd3caed93
amit-y11 <69119846+amit-y11@users.noreply.github.com>
2020-08-04 17:44:42 +0530

    Update bot.py
    Extracted text will be replied as a separate message

--- bot.py
+++ bot.py
@@ -43,7 +43,8 @@ def convert_image(update,context):
         api_response = api_instance.image_ocr_post(filename)
         print(api_response)
         confidence=api_response.mean_confidence_level
-        update.message.reply_text("Confidence level "+str(confidence)+" \nExtracted text:\n\n\n"+api_response.text_result)
+        update.message.reply_text("Accuracy : "+str(confidence)+" \nExtracted text:\n")
+        update.message.reply_text(api_response.text_result)
     except ApiException as e:
         update.message.reply_text("Exception when calling ImageOcrApi->image_ocr_photo_to_text: %s\n" % e)
         try:
