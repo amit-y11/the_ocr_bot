2c7d6fe962924fb1834f3ad761de242e17373cf5
amit-y11 <69119846+amit-y11@users.noreply.github.com>
2020-08-04 18:02:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ def convert_image(update,context):
         api_response = api_instance.image_ocr_post(filename)
         print(api_response)
         confidence=api_response.mean_confidence_level
-        update.message.reply_text("Accuracy : "+str(confidence*100)+" \nExtracted text:\n")
+        update.message.reply_text("Accuracy : "+str(confidence*100)+"% \nExtracted text:\n")
         update.message.reply_text(api_response.text_result)
     except ApiException as e:
         update.message.reply_text("Exception when calling ImageOcrApi->image_ocr_photo_to_text: %s\n" % e)
