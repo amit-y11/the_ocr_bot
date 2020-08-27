453e3c4a7126de8ac34f19242049706a3d1ffed8
amit-y11 <69119846+amit-y11@users.noreply.github.com>
2020-08-27 10:04:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ logger = logging.getLogger(__name__)
 def start(update,context):
     """Send a message when the command /start is issued."""
     global first
-    first=update.message.chat.first
+    first=update.message.chat.first_name
     update.message.reply_text('Hi! '+str(first)+' \n\nWelcome to Optical Character Recognizer Bot. \n\nJust send a clear image to me and i will recognize the text in the image and send it as a message!\nTo get my contact details tap /contact')
 
 @run_async
